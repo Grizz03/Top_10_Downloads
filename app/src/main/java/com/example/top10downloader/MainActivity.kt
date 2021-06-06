9b7e611746c04d0465a7d9a59643e6b7c13eacd5
grizz03 <sawilson1110@yahoo.com>
2021-06-06 12:31:25 -0700

    Async practice

--- app/src/main/java/com/example/top10downloader/MainActivity.kt
+++ app/src/main/java/com/example/top10downloader/MainActivity.kt
@@ -1,13 +1,37 @@
 package com.example.top10downloader
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG = "MainActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        Log.d(TAG, "onCreate called")
+        val downloadData = DownloadData()
+        downloadData.execute("URL goes here")
+        Log.d(TAG, "onCreate done")
+    }
 
+    companion object {
+        private class DownloadData : AsyncTask<String, Void, String>() {
+            private val TAG = "DownloadData"
+
+            override fun onPostExecute(result: String?) {
+                super.onPostExecute(result)
+                Log.d(TAG, "onPostExecute: parameter is $result")
+            }
+
+            override fun doInBackground(vararg params: String?): String {
+                Log.d(TAG, "doInBackground: starts with ${params[0]}")
+                return "doInBackground completed"
+            }
+        }
     }
 }
